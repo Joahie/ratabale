3f76a5d9ecaf0abc3cb4c60e961ecbeb399e32b2
Joahie <76458153+Joahie@users.noreply.github.com>
2022-08-28 08:21:33 -0700

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,4 @@
 const router = require('express').Router();
-const bodyParser = require('body-parser');
 const { render } = require('ejs');
 const { application } = require('express');
 const mongoclient = global.mongoclient;
